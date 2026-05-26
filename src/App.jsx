9bad12e43d89e71c1c39e71bc89dfa5fa0382a42
deepaktulera <deepakstuleraoo3@gmail.com>
2026-05-26 09:06:24 +0530

    Create Layout, NotFound component and style Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ import Navbar from './components/Navbar';
 import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
-  return (
-    <div className='w-screen h-screen px-4 py-2'>
-      <Navbar />
-      <AppRoutes />
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,15 +3,26 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import BrowseBooks from '../pages/BrowseBooks'
 import AddBook from '../pages/AddBook'
+import NotFound from '../pages/NotFound'
+
+import Layout from '../components/Layout'
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/browse_books' element={<BrowseBooks />} />
-        <Route path='/add_books' element={<AddBook />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+
+            {/* Routes with Navbar */}
+            <Route element={<Layout />}>
+                <Route path='/' element={<Home />} />
+                <Route path='/browse_books' element={<BrowseBooks />} />
+                <Route path='/add_books' element={<AddBook />} />
+            </Route>
+
+            {/* Route without Navbar */}
+            <Route path='*' element={<NotFound />} />
+
+        </Routes>
+    )
 }
 
 export default AppRoutes
